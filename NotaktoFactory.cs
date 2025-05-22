e97c8a03b84c2dca2c5003459368a37cf206a4e2
lmfjeff <lmfjeff@gmail.com>
2025-05-22 23:37:21 +1000

    bugfix: switch two factory

--- NotaktoFactory.cs
+++ NotaktoFactory.cs
@@ -3,8 +3,8 @@ using System;
 namespace a1;
 
 //Concrete implementation for Game
-public class NumTicTacToeFactory : IGameFactory
+public class NotaktoFactory : IGameFactory
 {
     public Game CreateGame(int boardSize, Player player1, Player player2)
-    { return new NumTicTacToeGame(boardSize, player1, player2);}
+    { return new NotaktoGame(boardSize, player1, player2); }
 }
--- NumTicTacToeFactory.cs
+++ NumTicTacToeFactory.cs
@@ -4,8 +4,8 @@ namespace a1;
 
 
 //Concrete implementation for Game
-public class NotaktoFactory : IGameFactory
+public class NumTicTacToeFactory : IGameFactory
 {
     public Game CreateGame(int boardSize, Player player1, Player player2)
-    { return new NotaktoGame(boardSize, player1, player2); }
+    { return new NumTicTacToeGame(boardSize, player1, player2);}
 }
